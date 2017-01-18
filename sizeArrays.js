857a84f2d0b5a600cfce8ca728146eaa4b2c60fb
brendo85 <brendan.feeney@gmail.com>
2017-01-18 17:42:45 +0000

    Commit sizeArrays.js to ApparelX

---
+++ sizeArrays.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+var sizeList = {};
+ 
+ sizeList = {
+            "Large":{ 
+                    "sizes":[
+                        { "Colours" : "Blue, Navy, Maroon",
+                        "Dimensions" : "Chest 42-44 inches" ,
+                        "Fair Factor" : "4 stars",  
+                        "Price" : "€120",                    
+                    }]},
+            "Medium":{ 
+                    "sizes":[
+                    {
+                        "Colours" : "Blue, Navy, Black",
+                        "Dimensions" : "Chest 40-42 inches" ,
+                        "Fair Factor" : "4 stars",  
+                        "Price" : "€120",
+                    }]},
+            "Small":{ 
+                    "sizes":[
+                    {
+                        "Colours" : "Blue, Navy, Maroon, Black",
+                        "Dimensions" : "Chest 38-40 inches" ,
+                        "Fair Factor" : "4 stars",  
+                        "Price" : "€120",
+                    }]}
+            };
